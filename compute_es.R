bc89002369adac568cdfbe4c517abb99461283de
cissard <cecile.issard@etu.parisdescartes.fr>
2020-10-02 14:13:36 +0200

    move resampling to data_completion

--- compute_es.R
+++ compute_es.R
@@ -24,7 +24,7 @@ compute_es <- function(participant_design, x_1 = NA, x_2 = NA, x_dif = NA,
 logOnError({
   assert_that(participant_design %in% c("between", "within_two", "within_one"))
 
-  #we introduce variables calles d_calc and d_var_calc to distiguish them from the fields d and d_var, which are fields where effect sizes were already available from the source of the data
+  #we introduce variables called d_calc and d_var_calc to distiguish them from the fields d and d_var, which are fields where effect sizes were already available from the source of the data
   d_calc <- NA
   d_var_calc <- NA
   es_method <- "missing"
